6a37d5c7ac285512087cfbcdc1145bbd82c56bfb
Aprisetiawan <apri7934@students.amikom.ac.id>
2016-12-09 10:22:18 +0700

    update usiswa1

--- usiswa.php
+++ usiswa.php
@@ -3,6 +3,8 @@ require_once('lib/DBClass.php');
 require_once('lib/m_siswa.php');
 require_once('lib/m_nationality.php');
 
+//update usiswa
+//wkwkwkwk
 $id = $_GET['id'];
 
 if (!is_numeric($id)) {
